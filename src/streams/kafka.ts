785fc7aaa3b8fb10abe68a12f8337f28c5952ecb
imagure <ricardo.imagure@gmail.com>
2023-05-26 11:28:11 -0300

    fix: adjust config reference for KAFKA client

--- src/streams/kafka.ts
+++ src/streams/kafka.ts
@@ -1,4 +1,4 @@
-import { Consumer, Kafka, Producer, SASLOptions, KafkaConfig} from 'kafkajs';
+import { Consumer, Kafka, Producer, SASLOptions, KafkaConfig } from 'kafkajs';
 import { LooseObject, ProduceParam, } from '../types';
 import { v4 as uuid, } from 'uuid';
 import { EachMessagePayload, } from 'kafkajs';
@@ -9,7 +9,7 @@ export class KafkaStream {
     _producer: Producer;
     _consumer: Consumer;
     _callback: any;
-  
+
     constructor(configs: LooseObject) {
         this._client = KafkaStream.createClient(configs);
         this._producer = this._client.producer();
@@ -18,32 +18,32 @@ export class KafkaStream {
         });
     }
 
-    async connect(){
+    async connect() {
         await this._consumer.connect();
         await this._producer.connect();
     }
 
-    static createClient(configs: LooseObject){
+    static createClient(configs: LooseObject) {
         const connectionConfig: KafkaConfig = {
             clientId: `${configs.CLIENT_ID}-${uuid()}`,
             brokers: [`${configs.BROKER_HOST}:${configs.BROKER_PORT}`],
         };
         if (configs.BROKER_KAFKA_MECHANISM && configs.BROKER_KAFKA_USERNAME && configs.BROKER_KAFKA_SECRET) {
             const sasl: SASLOptions = {
-                mechanism: configs.mechanism,
-                username: configs.username,
-                password: configs.pswd,
-            }; 
+                mechanism: configs.BROKER_KAFKA_MECHANISM,
+                username: configs.BROKER_KAFKA_USERNAME,
+                password: configs.BROKER_KAFKA_SECRET,
+            };
             connectionConfig.sasl = sasl;
             connectionConfig.ssl = !!sasl;
         }
         return new Kafka(connectionConfig);
     }
 
-    async setConsumer(topic: string, callback: any, dynamicKey = "$"){
+    async setConsumer(topic: string, callback: any, dynamicKey = "$") {
         this._callback = callback;
         let cleanTopic: string | RegExp = topic;
-        if(topic.indexOf(dynamicKey)!=-1) {
+        if (topic.indexOf(dynamicKey) != -1) {
             cleanTopic = new RegExp(topic.replace(dynamicKey, ".*"));
         }
         await this._consumer.subscribe({
@@ -52,13 +52,13 @@ export class KafkaStream {
         });
     }
 
-    async runConsumer(){
+    async runConsumer() {
         await this._consumer.run({
             eachMessage: this.mountConsumerCallback(this._callback),
         });
     }
 
-    async produce({ topic, message }: ProduceParam){
+    async produce({ topic, message }: ProduceParam) {
         const sendParams = {
             topic,
             messages: [{ value: JSON.stringify(message) }],
@@ -66,7 +66,7 @@ export class KafkaStream {
         await this._producer.send(sendParams);
     }
 
-    mountConsumerCallback(callback: any){
+    mountConsumerCallback(callback: any) {
         return async ({ topic, partition, message }: EachMessagePayload): Promise<void> => {
             const receivedMessage = message.value?.toString() || '';
             console.log(`[Kafka CONSUMER] Message received -> ${JSON.stringify(
